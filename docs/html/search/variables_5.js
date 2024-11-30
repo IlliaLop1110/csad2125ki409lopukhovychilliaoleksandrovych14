8059ba34d31b8566ff4accf0f5f800a2c7151e1c
IlliaLop1110 <ilopukhovich12345@gmail.com>
2024-11-30 16:44:48 +0200

    Delete docs directory

--- docs/html/search/variables_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['last_5freceived_5ftime_0',['last_received_time',['../namespacemain.html#a83307fce1cd0496d684a3b920d1e9976',1,'main']]]
-];
--- docs/html/search/variables_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ser_0',['ser',['../namespacemain.html#af0ddfa992c59910c3fdc03d171562f73',1,'main']]]
-];
